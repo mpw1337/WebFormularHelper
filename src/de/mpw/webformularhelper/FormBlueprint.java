84a43030cdc4fa617b5e0962703b8ad7fccc6363
Markus Wersig <m_wersig12@cs.uni-kl.de>
2015-07-07 13:15:15 +0200

    Extended support for easy Extension

---
+++ src/de/mpw/webformularhelper/FormBlueprint.java
@@ -0,0 +1,17 @@
+package de.mpw.webformularhelper;
+
+/**
+ * Created by Markus on 07.07.2015.
+ */
+public class FormBlueprint {
+    public final static String FIRSTNAME = "FirstName";
+    public final static String LASTNAME = "LastName";
+    public final static String EMAIL = "MemberName";
+    public static final String Password1 = "Password";
+    public static final String Password2 = "RetypePassword";
+    public static final String ZipCode = ""; //Inactive
+    public static final String BirthDay = "BirthDay";
+    public static final String BirthMonth = "BirthMonth";
+    public static final String BirthYear = "BirthYear";
+    public static final String Gender = "Gender";
+}
